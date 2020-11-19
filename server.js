be5bd7db6ee073adda60aab93b4d3060255b3621
Andres <anpaezros@gmail.com>
2020-11-19 20:13:09 +0100

    Added hola endpoints

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get("/", (req,res) => {
     res.status(200).send("Bienvenido a Shift left de valencia testing");
 })
 
+app.get("/hola", (req,res) => {
+    res.status(200).send("<h1>¡Hola VLCTesting!</h1>");
+});
+
+app.get("/hola/:name", (req, res) => {
+    const name = req.params.name;
+    res.status(200).send("<h1>¡Hola " + name + "!</h1>" )
+})
+
 http.createServer(app).listen(process.env.PORT || 8080), () => {
     console.log("Servidor iniciado");
 }
