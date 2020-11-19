b10c647b69ed33703566ac1c4e014bf259b5fa63
Andres <anpaezros@gmail.com>
2020-11-19 19:58:33 +0100

    fixed env port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/", (req,res) => {
     res.status(200).send("Bienvenido a Shift left de valencia testing");
 })
 
-http.createServer(app).listen(8080), () => {
-    console.log("Servidor iniciado en el puerto 8080");
+http.createServer(app).listen(process.env.PORT || 8080), () => {
+    console.log("Servidor iniciado");
 }
